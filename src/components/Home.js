75ac53162440642d6eb5cd17593a6f0ff0147672
luandevpro <luandevpro@gmail.com>
2018-12-23 14:31:27 +0700

    hoan tat xac thuc User

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { client } from "../config/stitch";
 
 export default class Home extends Component {
-  render() {
-    return (
-      <div>
-        homes
-      </div>
-    )
-  }
+	componentDidMount() {
+		var user = client.auth.user;
+		if (user) {
+			console.log(user);
+		} else {
+			console.log("ban phai login");
+		}
+	}
+	render() {
+		return <div>homes</div>;
+	}
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { UserPasswordCredential } from "mongodb-stitch-browser-sdk";
+import { Formik } from "formik";
+import FormField from "./FormField";
+import { client } from "../config/stitch";
 
 export default class Login extends Component {
-  render() {
-    return (
-      <div>
-        login
-      </div>
-    )
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: "",
+			password: "",
+		};
+	}
+	handleSubmit = values => {
+		const credential = new UserPasswordCredential(
+			values.email,
+			values.password
+		);
+		client.auth
+			.loginWithCredential(credential)
+			.then(authedId => {
+				console.log(`successfully logged in with id`, authedId);
+			})
+			.catch(err => console.error(`login failed with error: ${err}`));
+	};
+	render() {
+		var { email, password } = this.state;
+		return (
+			<Formik
+				initialValues={{ email, password }}
+				onSubmit={this.handleSubmit}
+			>
+				{props => <FormField {...props} />}
+			</Formik>
+		);
+	}
 }
